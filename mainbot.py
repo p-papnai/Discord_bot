431bb167892f5826332958b34df4eb96e892b223
p-papnai <70071875+p-papnai@users.noreply.github.com>
2020-10-10 20:16:29 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -1,1 +1,40 @@
 #main code for discord bot
+# bot.py
+import os
+import random
+
+
+
+import discord
+from dotenv import load_dotenv
+
+
+load_dotenv()
+'''TOKEN = os.getenv(DISCORD_TOKEN)'''
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if 'happy birthday' in message.content.lower():
+        await message.channel.send('Happy Birthday! 🎈🎉')
+  
+
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+
+client.run(TOKEN)
